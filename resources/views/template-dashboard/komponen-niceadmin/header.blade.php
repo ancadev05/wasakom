8df39432433ebb7c8c1a799b6e70c4029d9dc6ae
hdev05 <hamzah.dev.061223@gmail.com>
2024-07-18 19:02:54 +0800

    mengubah label header

--- resources/views/template-dashboard/komponen-niceadmin/header.blade.php
+++ resources/views/template-dashboard/komponen-niceadmin/header.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="#" class="logo d-flex align-items-center">
         <img src="{{ asset('assets/img/logo-wana.png') }}" alt="">
-        <span class="d-none d-lg-block">Wasakom</span>
+        <span class="d-none d-lg-block">Teknisi Wana</span>
       </a>
     </div><!-- End Logo -->
     <i class="bi bi-list toggle-sidebar-btn"></i>
